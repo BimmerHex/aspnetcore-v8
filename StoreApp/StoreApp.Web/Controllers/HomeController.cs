356960c71c3430d413ca4422f0947f04010c7d1f
BimmerHex <github@vintagegamestudio.com>
2024-03-07 04:50:35 +0300

    Product List oluşturuldu

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using StoreApp.Data.Abstract;
+using StoreApp.Web.Models;
 
 namespace StoreApp.Web.Controllers;
 
@@ -12,5 +13,17 @@ public class HomeController : Controller
         _storeRepository = storeRepository;
     }
 
-    public IActionResult Index() => View();
+    public IActionResult Index()
+    {
+        var products = _storeRepository.Products.Select(p => new ProductViewModel {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            Price = p.Price
+        }).ToList();
+
+        return View(new ProductListViewModel {
+            Products = products
+        });
+    }
 }
